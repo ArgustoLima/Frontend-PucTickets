9efdf0e2a9b0e3e8dc439dffe06f998a37d0b059
Argusto Lima <augusto.vlima2@gmai.com>
2023-06-05 18:53:57 -0300

    Subindo primeiro teste do Front do PucTicket

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,11 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { PaginaInicial } from '../pages/PaginaInicial';
 import { Login } from '../pages/Login';
 import { Cadastro } from '../pages/Cadastro';
-import { TelaUsuario } from '../pages/TelaUsuario';
-import { CadastroEndereco } from '../pages/CadastroEndereco';
-import { TelaMotorista } from '../pages/TelaMotorista';
-import { CadastroCarro } from '../pages/CadastroCarro';
-import { Feedback } from '../pages/Feedback';
+
 
 export const AppRouter = () =>{
     return(
@@ -16,11 +12,7 @@ export const AppRouter = () =>{
                 <Route path="/" exact element={<PaginaInicial />} />
                 <Route path="/login" exact element={<Login />} />
                 <Route path="/cadastro" exact element={<Cadastro />} />
-                <Route path="/tela_usuario" exact element={<TelaUsuario />} />
-                <Route path="/cadastro_carro" exact element={<CadastroCarro />} />
-                <Route path="/cadastro_endereco" exact element={<CadastroEndereco />} />    
-                <Route path="/tela_motorista" exact element={<TelaMotorista />} />      
-                <Route path="/feedback" exact element={<Feedback />} />           
+                     
             </Routes>
         </Router>
     );
